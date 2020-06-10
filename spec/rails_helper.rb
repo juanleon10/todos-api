ba1bc4069b5689fcfa26c4883e258c419ac958e9
Juan leon <leon@condo-media.com>
2020-06-10 09:33:22 -0500

    Creando el servicio de token

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -86,6 +86,8 @@ RSpec.configure do |config|
     end
   end
 
-  config.include RequestSpecHelper, type: :request
+  #config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
 
 end
